838e6b8732ec58faf00605c7ca354dd120acd90f
Wiktor Frątczak <01171649@pw.edu.pl>
2024-12-25 03:40:24 +0100

    add abstract class for product_repository.py

--- backend/application/product/product_repository.py
+++ backend/application/product/product_repository.py
@@ -1,52 +1,28 @@
-import gridfs
-from pymongo import MongoClient
-from bson import ObjectId
-import os
-from dotenv import load_dotenv
+import abc
 from typing import Optional
 
-load_dotenv()
-mongo_password = os.getenv("MONGO_PASSWORD")
-mongo_user = os.getenv("MONGO_USER")
-client = MongoClient(f"mongodb+srv://{mongo_user}:{mongo_password}@paim.yxyyk.mongodb.net/")
-db = client["product"]
-fs = gridfs.GridFS(db)
-
-def upload_product_to_db(
-    name: str,
-    price: float,
-    country_of_origin: str,
-    description: str,
-    fruit_or_vegetable: str,
-    expiry_date: str,
-    image_data: bytes,
-) -> str:
-    file_id = fs.put(image_data)
-
-    product_data = {
-        "name": name,
-        "price": price,
-        "country_of_origin": country_of_origin,
-        "description": description,
-        "fruit_or_vegetable": fruit_or_vegetable,
-        "imageId": str(file_id),
-        "expiry_date": expiry_date
-    }
-
-    product_collection = db["products"]
-    product_id = product_collection.insert_one(product_data).inserted_id
-
-    return str(product_id)
-
-def get_product_from_db(product_id: str) -> Optional[dict]:
-    product_collection = db["products"]
-    product = product_collection.find_one({"_id": ObjectId(product_id)})
-    return product
+import gridfs
 
 
-def get_image_from_db(image_id: str) -> Optional[gridfs.GridOut]:
-    try:
-        image_id = ObjectId(image_id)
-        return fs.get(image_id)
-    except Exception:
-        return None
+class AbstractProductRepository(abc.ABC):
+
+    @abc.abstractmethod
+    def upload_product_to_db(
+            self,
+            name: str,
+            price: float,
+            country_of_origin: str,
+            description: str,
+            fruit_or_vegetable: str,
+            expiry_date: str,
+            image_data: bytes,
+    ) -> str:
+        pass
+
+    @abc.abstractmethod
+    def get_product_from_db(self, product_id: str) -> Optional[dict]:
+        pass
+
+    @abc.abstractmethod
+    def get_image_from_db(self, image_id: str) -> Optional[gridfs.GridOut]:
+        pass
---
+++ backend/infrastructure/mongo/product_repository.py
@@ -0,0 +1,57 @@
+import gridfs
+from pymongo import MongoClient
+from bson import ObjectId
+import os
+from dotenv import load_dotenv
+from typing import Optional
+
+from application.product.product_repository import AbstractProductRepository
+
+load_dotenv()
+mongo_password = os.getenv("MONGO_PASSWORD")
+mongo_user = os.getenv("MONGO_USER")
+
+class ProductRepositoryMongo(AbstractProductRepository):
+    def __init__(self):
+        self.client = MongoClient(f"mongodb+srv://{mongo_user}:{mongo_password}@paim.yxyyk.mongodb.net/")
+        self.db = self.client["client"]
+        self.fs = gridfs.GridFS(self.db)
+
+    def upload_product_to_db(self,
+        name: str,
+        price: float,
+        country_of_origin: str,
+        description: str,
+        fruit_or_vegetable: str,
+        expiry_date: str,
+        image_data: bytes,
+    ) -> str:
+        file_id = self.fs.put(image_data)
+
+        product_data = {
+            "name": name,
+            "price": price,
+            "country_of_origin": country_of_origin,
+            "description": description,
+            "fruit_or_vegetable": fruit_or_vegetable,
+            "imageId": str(file_id),
+            "expiry_date": expiry_date
+        }
+
+        product_collection = self.db["products"]
+        product_id = product_collection.insert_one(product_data).inserted_id
+
+        return str(product_id)
+
+    def get_product_from_db(self, product_id: str) -> Optional[dict]:
+        product_collection = self.db["products"]
+        product = product_collection.find_one({"_id": ObjectId(product_id)})
+        return product
+
+
+    def get_image_from_db(self, image_id: str) -> Optional[gridfs.GridOut]:
+        try:
+            image_id = ObjectId(image_id)
+            return self.fs.get(image_id)
+        except Exception:
+            return None
