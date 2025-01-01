3f7b508da376509f9cfa46b1147498e678e8b74c
Wiktor Frątczak <01171649@pw.edu.pl>
2025-01-01 18:00:13 +0100

    move db name to .env file

--- backend/infrastructure/mongo/client_repository.py
+++ backend/infrastructure/mongo/client_repository.py
@@ -9,20 +9,19 @@ from infrastructure.mongo.mongo_client import MongoDBClient
 
 class ClientRepositoryMongo(AbstractClientRepository):
     def __init__(self):
-        self.db = MongoDBClient.get_database("shop_db")
+        self.client_collection = MongoDBClient.get_collection("clients")
 
     def register_client_db(self, client: Client) -> ClientResponse:
-        client_collection = self.db["clients"]
         client_data = client.model_dump()
-        client_data["id"] = str(client_collection.insert_one(client_data).inserted_id)
+        client_data["id"] = str(self.client_collection.insert_one(client_data).inserted_id)
         return ClientResponse(**client_data)
 
     def get_client_db(self, client_id: str) -> ClientResponse:
-        client_collection = self.db["clients"]
-        client_data = client_collection.find_one({"_id": ObjectId(client_id)})
+        client_data = self.client_collection.find_one({"_id": ObjectId(client_id)})
 
         if not client_data:
             raise ClientNotFoundError(client_id)
+
         client_data["id"] = str(client_data["_id"])
 
         return ClientResponse(**client_data)
--- backend/infrastructure/mongo/product_repository.py
+++ backend/infrastructure/mongo/product_repository.py
@@ -10,22 +10,21 @@ from infrastructure.mongo.mongo_client import MongoDBClient
 class ProductRepositoryMongo(AbstractProductRepository):
 
     def __init__(self):
-        self.db = MongoDBClient.get_database("shop_db")
+        self.product_collection = MongoDBClient.get_collection("products")
 
     async def upload_product_to_db(self, product: Product) -> ProductResponse:
-        product_collection = self.db["products"]
         image_data = await product.file.read()
 
         product_data = product.model_dump()
         product_data["file"] = image_data
-        result = product_collection.insert_one(product_data)
+        result = self.product_collection.insert_one(product_data)
         product_data["file"] = str(image_data)
         product_data["id"] = str(result.inserted_id)
         return ProductResponse(**product_data)
 
     def get_product_by_id(self, product_id: str) -> ProductResponse:
-        product_collection = self.db["products"]
-        product = product_collection.find_one({"_id": ObjectId(product_id)})
+        product = self.product_collection.find_one({"_id": ObjectId(product_id)})
+
         if not product:
             raise ProductNotFoundError(product_id)
         return ProductResponse(**product)
