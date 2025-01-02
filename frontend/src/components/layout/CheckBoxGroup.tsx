c2c420da894016e9d40b694712dc3850ef2fac54
Wiktor Frątczak <01171649@pw.edu.pl>
2025-01-02 19:39:09 +0100

    fix sidebar component margin

--- frontend/src/components/layout/CheckBoxGroup.tsx
+++ frontend/src/components/layout/CheckBoxGroup.tsx
@@ -2,23 +2,23 @@ import React from "react";
 import { Box, Checkbox, FormControlLabel, Typography } from "@mui/material";
 
 interface CheckboxGroupProps {
-    title: string;
-    options: string[];
+  title: string;
+  options: string[];
 }
 
 const CheckboxGroup: React.FC<CheckboxGroupProps> = ({ title, options }) => {
     return (
-        <Box sx={{ display: "flex", flexDirection: "column", mb: 2, alignItems: "center" }}>
-            <Typography variant="h6" sx={{ mb: 1, textAlign: "center" }}>
+        <Box sx={{ mb: 2, marginLeft:"1rem" }}>
+            <Typography variant="h6" sx={{ mb: 1 }}>
                 {title}
             </Typography>
-            <Box sx={{ display: "flex", gap: 2, justifyContent: "center" }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
                 {options.map((option, index) => (
                     <FormControlLabel
                         key={index}
                         control={<Checkbox />}
                         label={option}
-                        sx={{ display: "inline-flex", alignItems: "center" }}
+                        sx={{ display: 'inline-flex', alignItems: 'center' }}
                     />
                 ))}
             </Box>
